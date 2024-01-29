e262e63f85923e209bb3b2334601b8e45f5dac26
zhenyee92 <zhenyee6660@gmail.com>
2024-01-29 20:29:06 +0800

    更新HTML

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 // 配置 Express 来解析 JSON 格式的请求体
 app.use(express.json());
 
+// 使用 express.static 中间件来提供静态文件
+app.use(express.static(path.join(__dirname)));
+
 // 处理 GET 请求到 /login 的路由，发送登录页面
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '../Muduo/login.html'));
+    const filePath = path.join(__dirname, 'login.html');
+    res.sendFile(filePath);
 });
+
 
 // 处理 POST 请求到 /login 的路由，用于登录逻辑
 app.post('/login', (req, res) => {
@@ -18,16 +27,14 @@ app.post('/login', (req, res) => {
 
     // 简单的登录验证逻辑
     if (username === "admin" && password === "555888") {
-        // 登录成功
         res.status(200).send("登录成功！");
     } else {
-        // 登录失败
         res.status(401).send("用户名或密码错误");
     }
 });
 
 // 配置服务器监听端口
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
